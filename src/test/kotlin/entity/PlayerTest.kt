da18537794af3e32526dbddbeb70a8c5c175da24
Mohamad.Abdo <mohamad.abdo@tu-dortmund.de>
2022-07-26 21:26:06 +0200

    implemented PlayerTest.kt and SwimAppTest.kt

---
+++ src/test/kotlin/entity/PlayerTest.kt
@@ -0,0 +1,36 @@
+package entity
+
+import kotlin.test.*
+
+class PlayerTest {
+    private val deck1 = Deck(
+        ArrayDeque(
+            listOf(
+                Card(CardSuit.DIAMONDS, CardValue.QUEEN),
+                Card(CardSuit.DIAMONDS, CardValue.KING),
+                Card(CardSuit.DIAMONDS, CardValue.TEN)
+            )
+        )
+    )
+    private val deck2 = Deck(
+        ArrayDeque(
+            listOf(
+                Card(CardSuit.HEARTS, CardValue.QUEEN),
+                Card(CardSuit.HEARTS, CardValue.KING),
+                Card(CardSuit.HEARTS, CardValue.TEN)
+            )
+        )
+    )
+    val player1 = Player("Mo",deck1)
+    val player2 = Player("Luca",deck2)
+
+    @Test
+    fun testPlayer(){
+        assert(player1.playerName == "Mo")
+        assert(player1.playerHand == deck1)
+        assert(player2.playerName == "Luca")
+        assert(player2.playerHand == deck2)
+
+    }
+
+}
---
+++ src/test/kotlin/entity/SwimAppTest.kt
@@ -0,0 +1,82 @@
+package entity
+
+import kotlin.test.*
+
+class SwimAppTest{
+
+    private val pile = Deck(
+        ArrayDeque(
+            listOf(
+                Card(CardSuit.DIAMONDS, CardValue.QUEEN),
+                Card(CardSuit.DIAMONDS, CardValue.KING),
+                Card(CardSuit.DIAMONDS, CardValue.TEN)
+            )
+        )
+    )
+    private val d2 = Deck(
+        ArrayDeque(
+            listOf(
+                Card(CardSuit.HEARTS, CardValue.QUEEN),
+                Card(CardSuit.HEARTS, CardValue.KING),
+                Card(CardSuit.HEARTS, CardValue.TEN)
+            )
+        )
+    )
+    private val d3 = Deck(
+        ArrayDeque(
+            listOf(
+                Card(CardSuit.SPADES, CardValue.QUEEN),
+                Card(CardSuit.SPADES, CardValue.KING),
+                Card(CardSuit.SPADES, CardValue.TEN)
+            )
+        )
+    )
+    private val table = Deck(
+        ArrayDeque(
+            listOf(
+                Card(CardSuit.CLUBS, CardValue.QUEEN),
+                Card(CardSuit.CLUBS, CardValue.KING),
+                Card(CardSuit.CLUBS, CardValue.TEN)
+            )
+        )
+    )
+    private val d1 = Deck(
+        ArrayDeque(
+            listOf(
+                Card(CardSuit.SPADES, CardValue.SEVEN),
+                Card(CardSuit.SPADES, CardValue.NINE),
+                Card(CardSuit.SPADES, CardValue.EIGHT)
+            )
+        )
+    )
+
+    val p1 = Player("Mo",d1)
+    val p2 = Player("Hasan",d2)
+    val p3 = Player("Safi",d3)
+    val playersList = listOf<Player>(p1,p2,p3)
+    var currentPlayer = p1
+    val calledPlayer = currentPlayer
+    var passes: Int = 0
+
+
+    @Test
+    fun testSwimApp(){
+        assert(playersList[0]==p1)
+        assert(playersList[1]==p2)
+        assert(playersList[2]==p3)
+
+        assert(currentPlayer==p1)
+        assert(calledPlayer==p1)
+        assert(passes==0)
+
+        val pileCopy = pile.copy()
+        assert(pileCopy==pile)
+
+
+        val tableCopy = table.copy()
+        assert(tableCopy==table)
+
+    }
+
+
+}
