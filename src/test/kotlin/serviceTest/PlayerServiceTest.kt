0ca735f9c3b0c4b349d2f6ec44744013c1a0085c
Mohamad.Abdo <mohamad.abdo@tu-dortmund.de>
2022-07-29 16:51:03 +0200

    created PlayerServiceTest.kt

---
+++ src/test/kotlin/serviceTest/PlayerServiceTest.kt
@@ -0,0 +1,40 @@
+package serviceTest
+
+import org.junit.jupiter.api.Test
+import service.RootService
+
+
+/**
+ * Class that provides tests for [PlayerServiceTest]
+ */
+class PlayerServiceTest {
+
+    private val rootService = RootService()
+    private val playerService = rootService.playerService
+    private val refreshablesTest = RefreshablesTest()
+
+    @Test
+    fun testPass() {
+
+    }
+
+    @Test
+    fun testCall() {
+
+    }
+
+    @Test
+    fun testSwapOneCard() {
+
+    }
+
+    @Test
+    fun testSwapAllCards() {
+
+    }
+
+    @Test
+    fun testCalculatePoints() {
+
+    }
+}
