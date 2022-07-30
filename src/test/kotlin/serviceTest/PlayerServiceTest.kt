ca29575160e7ecc4a1749781b2acdef8f6e4fb41
Mohamad.Abdo <mohamad.abdo@tu-dortmund.de>
2022-07-30 16:34:12 +0200

    implemented testSwapAllCards

--- src/test/kotlin/serviceTest/PlayerServiceTest.kt
+++ src/test/kotlin/serviceTest/PlayerServiceTest.kt
@@ -28,6 +28,10 @@ class PlayerServiceTest {
         swimService.startGame()
     }
 
+
+    /**
+     * test if the function pass works correctly
+     */
     @Test
     fun testPass() {
         assertEquals(0, game.passes)
@@ -36,6 +40,10 @@ class PlayerServiceTest {
         assertEquals(2, game.passes)
     }
 
+
+    /**
+     * test if the function call works correctly
+     */
     @Test
     fun testCall() {
         game.passes = 1
@@ -45,6 +53,10 @@ class PlayerServiceTest {
         assert(game.passes == 0)
     }
 
+
+    /**
+     * test if the function swapOneCard works correctly
+     */
     @Test
     fun testSwapOneCard() {
         val playerCard = game.currentPlayer.playerHand.cards[0]
@@ -53,11 +65,31 @@ class PlayerServiceTest {
         //println(tableCard.toString())
     }
 
+    /**
+     * test if the function swapAllCards works correctly
+     */
     @Test
     fun testSwapAllCards() {
+        val hand = game.currentPlayer.playerHand.cards
+        val table = game.tableDeck.cards
+
+
+        //check if the variables assigned correctly
+        assertEquals(hand,game.currentPlayer.playerHand.cards)
+        assertEquals(table,game.tableDeck.cards)
+
+        playerService.swapAllCards()
+
+        //check if swapped correctly
+        assertEquals(table,game.currentPlayer.playerHand.cards)
+        assertEquals(hand,game.tableDeck.cards)
 
     }
 
+
+    /**
+     * test if the function calculatePoints works correctly
+     */
     @Test
     fun testCalculatePoints() {
         for (i in game.players){
